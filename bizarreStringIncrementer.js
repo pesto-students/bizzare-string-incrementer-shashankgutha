e3c07880f23ad000a21c4c742349ec2b9a327d4b
shashank gutha <shashankgutha@gmail.com>
2020-12-14 17:40:58 +0530

    Added bizarre increment Function

--- bizarreStringIncrementer.js
+++ bizarreStringIncrementer.js
@@ -1,1 +1,31 @@
-// Start your implementation here
+function bizarreStringIncrementer(inputChar){
+  var inputNumber="";
+  var outputChar =0;
+  var zeroCount=0;
+  var charStrIdx=0;
+  for (var i = inputChar.length; i > 0; i--) {
+    var tempc=inputChar.charAt(i-1);
+    if(!isNaN(tempc)){
+        inputNumber=inputChar.charAt(i-1)+inputNumber;
+        if(inputChar.charAt(i-1) === "0"){
+          zeroCount++;   
+        }
+    }else{
+      charStrIdx=i;
+      break;
+    }
+}
+if(inputNumber!="" && !isNaN(inputNumber)){
+  outputChar= parseInt(inputNumber)+1;
+  outputChar = outputChar+"";
+  var newZeros=inputNumber.length-outputChar.length;
+  if(newZeros <= zeroCount  ){
+    for(var i1=0; i1<newZeros;i1++){
+        outputChar="0"+outputChar;
+    }
+  }
+  return inputChar.substring(0, charStrIdx)+outputChar;
+}else{
+  return inputChar+"1";
+}
+}
